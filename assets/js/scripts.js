db7c5f0f46deb271b5605f07374596071f30ccd5
Andre Firchow <andre@firchow.net>
2015-10-09 14:28:13 +0200

    Added some classes, text helpers and examples
    Extend some classes and text helpers. Added example page for parallax
    footer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,12 @@
+document.getElementById('page-top').className = 'js';
 
 $(function(){
 
     console.log('jQuery is ready.');
+
+    var isTouch = (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
+    if(isTouch) { $('html').addClass('touch') }
+
+
 
 });
